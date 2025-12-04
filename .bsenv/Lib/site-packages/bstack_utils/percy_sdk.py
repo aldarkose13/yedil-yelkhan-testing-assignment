8b7679eeb6e64b42cd905c0ee52dc101436c6e55
Yedil Yelkhan <115059814+aldarkose13@users.noreply.github.com>
2025-12-04 19:27:59 +0500

    Delete .bsenv directory

--- .bsenv/Lib/site-packages/bstack_utils/percy_sdk.py
+++
@@ -1,8 +0,0 @@
-import os
-from percy import percy_screenshot
-from bstack_utils.constants import STAGE, EVENTS
-from bstack_utils.measure import measure
-class PercySDK:
-  @classmethod
-  def screenshot(cls,driver, name, **kwargs):
-    percy_screenshot(driver, name, **kwargs)
